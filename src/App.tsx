5ba599ecfebdf50763042678c61dd2f67007921a
Himanshu Sinha <ashusinha1904@gmail.com>
2024-12-20 19:46:55 +0530

    Shadcn setup complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 const App = () => {
   return (
     <div>
       <p className="text-xl text-red-500 font-extrabold">App</p>
+      <Button>Click Me</Button>
     </div>
   );
 };
